dc8dfa8d3aa60c0267df33c1e5d6e7ce4c0a189c
augsi <augsi97@web.de>
2021-11-12 10:54:53 +0100

    test

--- le65dyem/function.py
+++ le65dyem/function.py
@@ -3,4 +3,16 @@ import ipywidgets import interact, fixed
 import PIL  import Image
 
 def imshow(X, resize = None):
-    pass
+    
+    img = Image.open(X)
+    
+    
+    wpercent = (resize/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    
+    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    
+    img.show()
+    
+    
+ipywidgets.interact(imshow, resize=(0,2000))
